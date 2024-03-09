6569bef67a4612622696bd381468abf43174c138
helindevani <helindevani111@gmail.com>
2024-03-09 11:45:00 +0530

    compele dependency injection in .netcore

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using ServiceContracts;
+namespace DIExample.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        //constructer injection
+       /* private readonly ICitiesService _citiesService;
+
+        public HomeController(ICitiesService citiesService)
+        {
+            _citiesService=citiesService;
+        }
+       */
+       //method injection
+        [Route("/")]
+        public IActionResult Index([FromServices] ICitiesService citiesService1,[FromServices]ICitiesService citiesService2,[FromServices] ICitiesService citiesService3, [FromServices] IServiceScopeFactory serviceScopeFactory)
+        {
+
+			List<string> cities =citiesService1.GetCities();
+            ViewBag.Instanceid1=citiesService1.ServiceInstanceId;
+			ViewBag.Instanceid2 =citiesService2.ServiceInstanceId;
+			ViewBag.Instanceid3=citiesService3.ServiceInstanceId;
+
+
+            using(IServiceScope scope = serviceScopeFactory.CreateScope())
+            {
+                ICitiesService citiesService = scope.ServiceProvider.GetService<ICitiesService>();
+
+                ViewBag.citiservice_scope = citiesService.ServiceInstanceId;
+            }
+			return View(cities);
+        }
+    }
+}
---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,40 @@
+﻿using ServiceContracts;
+namespace Services
+{
+    public class CitiesService : ICitiesService, IDisposable
+    {
+        private List<string> _cities;
+
+        private Guid _serviceInstanceId;
+        public Guid ServiceInstanceId
+        {
+            get
+            {
+                return _serviceInstanceId;
+            }
+        }
+
+        public CitiesService()
+        {
+			_serviceInstanceId = Guid.NewGuid();
+            _cities = new List<string>()
+            {
+                "London",
+                "Paris",
+                "New York",
+                "Tokyo",
+                "Rome"
+            };   
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+
+		public void Dispose()
+		{
+	
+		}
+	}
+}
